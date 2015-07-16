17ed1ae54df71c1148226b1f8871cd0f99a4a59f
Jeff Meister <nanaki@gmail.com>
2015-07-16 12:58:29 -0700

    More parsing

--- icalendar_recur.ml
+++ icalendar_recur.ml
@@ -1,34 +1,33 @@
 open Batteries
 open Icalendar_t
 
-let parse_freq f = assert false
+(* Utility function for Atdgen validation *)
+let check f x =
+  match f [] x with
+  | None -> x
+  | Some err ->
+      raise (Invalid_argument (Ag_util.Validation.string_of_error err))
 
-let parse_byseclist l = assert false
+let positive_int_of_string s =
+  let n = int_of_string s in
+  check Icalendar_v.validate_positive_int n
 
-let parse_byminlist l = assert false
+(* Simple comma-separated lists of numbers *)
+let parse_int_list valid l =
+  List.map (fun s ->
+    let n = int_of_string s in
+    check valid n
+  ) (String.nsplit l ~by:",")
 
-let parse_byhrlist l = assert false
+let rec parse (rrule : string) : recur =
+  List.map parse_recur_rule_part (String.nsplit rrule ~by:";")
 
-let parse_bywdaylist l = assert false
-
-let parse_bymodaylist l = assert false
-
-let parse_byyrdaylist l = assert false
-
-let parse_bywknolist l = assert false
-
-let parse_bymolist l = assert false
-
-let parse_bysplist l = assert false
-
-let parse_weekday day = assert false
-
-let parse_recur_rule_part p =
+and parse_recur_rule_part p =
   match String.split p ~by:"=" with
   | ("FREQ", f) -> `Freq (parse_freq f)
   | ("UNTIL", date) -> `Until (Util_time.of_string date)
-  | ("COUNT", n) -> `Count (int_of_string n)
-  | ("INTERVAL", n) -> `Interval (int_of_string n)
+  | ("COUNT", n) -> `Count (positive_int_of_string n)
+  | ("INTERVAL", n) -> `Interval (positive_int_of_string n)
   | ("BYSECOND", l) -> `Bysecond (parse_byseclist l)
   | ("BYMINUTE", l) -> `Byminute (parse_byminlist l)
   | ("BYHOUR", l) -> `Byhour (parse_byhrlist l)
@@ -42,8 +41,53 @@ let parse_recur_rule_part p =
   | (name, value) ->
       raise (Invalid_argument ("Unrecognized recur_rule_part " ^ p))
 
-let parse (rrule : string) : recur =
-  List.map parse_recur_rule_part (String.nsplit rrule ~by:";")
+and parse_freq = function
+  | "SECONDLY" -> `Secondly
+  | "MINUTELY" -> `Minutely
+  | "HOURLY" -> `Hourly
+  | "DAILY" -> `Daily
+  | "WEEKLY" -> `Weekly
+  | "MONTHLY" -> `Monthly
+  | "YEARLY" -> `Yearly
+  | s -> raise (Invalid_argument ("Unrecognized freq " ^ s))
+
+and parse_byseclist l = parse_int_list Icalendar_v.validate_seconds l
+
+and parse_byminlist l = parse_int_list Icalendar_v.validate_minutes l
+
+and parse_byhrlist l = parse_int_list Icalendar_v.validate_hour l
+
+and parse_weekdaynum s =
+  let ordwk_char = ['0'; '1'; '2'; '3'; '4'; '5'; '6';
+                    '7'; '8'; '9'; '0'; '+'; '-'] in
+  if List.exists (String.contains s) ordwk_char then
+    Scanf.sscanf s "%d%s" (fun n wday ->
+      let ordwk = check Icalendar_v.validate_ordwk n in
+      (Some ordwk, parse_weekday wday)
+    )
+  else (None, parse_weekday s)
+
+and parse_bywdaylist l = assert false
+
+and parse_bymodaylist l = assert false
+
+and parse_byyrdaylist l = assert false
+
+and parse_bywknolist l = assert false
+
+and parse_bymolist l = assert false
+
+and parse_bysplist l = assert false
+
+and parse_weekday = function
+  | "SU" -> `Sunday
+  | "MO" -> `Monday
+  | "TU" -> `Tuesday
+  | "WE" -> `Wednesday
+  | "TH" -> `Thursday
+  | "FR" -> `Friday
+  | "SA" -> `Saturday
+  | s -> raise (Invalid_argument ("Unrecognized weekday " ^ s))
 
 let print (rrule : recur) : string = "" (* TODO *)
 
